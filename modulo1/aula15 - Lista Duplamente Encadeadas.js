462e724e983722ef5dadd7b19e6d7e510705364e
Raiana-ap <raianaapaludo@gmail.com>
2025-07-28 01:44:18 -0300

    inicio do remover feito + comentários

--- modulo1/aula15 - Lista Duplamente Encadeadas.js
+++ modulo1/aula15 - Lista Duplamente Encadeadas.js
@@ -7,57 +7,68 @@
 // double encoded list ao invez de só ter um próximo também apresenta um anterior, permitindo navegar por ambos sentidos 
 
 // EX
-class Node {
+class Node { //o construtor é responsável por criar cada elemento da lista encadeada
     constructor(valor) {
         this.valor = valor
         this.anterior = null
         this.proximo = null
-    } 
+    }
 }
 
 class DoublyLinkedList {
-    constructor() {
+    constructor() { // controla todos os nós - "gerente"
         this.head = null
         this.tail = null // último elemento da lista
         this.length = 0 // usado para saber tamanho
     }
 
     inserirInicio(valor) {
-        let novo = new Node(valor)
-        if(!this.head){
+        let novo = new Node(valor) // Cria um novo nó com o valor informado.
+        if (!this.head) {
+            //novo nó será o único da lista, então tanto o head quanto o tail apontam para ele.
             this.head = this.tail = novo
-        }else{
+        } else {
             novo.proximo = this.head
             this.head.anterior = novo
-            this.head = novo      
+            this.head = novo
         }
         this.length++
+
+        /*Exemplo:
+            Lista atual: [20] -> [30]
+            Com head = 20 e tail = 30
+            Chamando inserirInicio(10):
+                Cria [10]
+                    Faz 10 -> 20
+                    Faz 20 <- 10
+                    head agora é 10
+            Nova lista: [10] <-> [20] <-> [30] */
     }
 
     inserirFim(valor) {
         let novo = new Node(valor)
-        if(!this.head){
+        if (!this.head) {
             this.head = this.tail = novo
-        }else{
+        } else {
             this.tail.proximo = novo
             novo.anterior = this.tail
-            this.tail = novo      
+            this.tail = novo
         }
         this.length++
     }
 
-    inserirEm(indice, valor){ // consigo inserir em qualquer possicao da lista
-        if(indice<0 || indice > this.length)throw RangeError 
-        ("ïndice está fora dos limites!")
+    inserirEm(indice, valor) { // consigo inserir em qualquer possicao da lista
+        if (indice < 0 || indice > this.length) throw RangeError
+            ("ïndice está fora dos limites!")
         if (indice === 0) return this.inserirInicio(valor)
         if (indice === this.length) return this.inserirFim(valor)
 
         let atual = this.head
-        for(let i = 0; i < indice; i++){
+        for (let i = 0; i < indice; i++) {
             atual = atual.proximo
         }
 
-        
+
         let novo = new Node(valor)
         let noAnterior = atual.anterior
         noAnterior.proximo = novo
@@ -65,9 +76,11 @@ class DoublyLinkedList {
         novo.proximo = atual
         atual.anterior = novo
 
-         this.length++
+        this.length++
     }
-    removerFim(indice){
+    removerFim(indice) {
+      if (indice < 0 || indice > this.length) throw RangeError
+            ("ïndice está fora dos limites!")  
         
     }
 }
