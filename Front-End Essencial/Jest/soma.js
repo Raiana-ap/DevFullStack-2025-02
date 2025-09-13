59975a0ef2a3ca717a590630332d723fe3523ffd
Raiana Arbusti <raianaapaludo@gmail.com>
2025-09-13 18:41:01 -0300

    parte inicial de teste unitários com jest

---
+++ Front-End Essencial/Jest/soma.js
@@ -0,0 +1,11 @@
+function soma(a, b){
+        return a + b
+}
+
+module.exports = soma
+
+
+// PASSOS
+// 1. npm init -y
+// 2. npm install jest e mudar test do package para jest
+// 3. npm test
---
+++ Front-End Essencial/Jest/soma.test.js
@@ -0,0 +1,4 @@
+const soma = require('./soma')
+test('Soma 2 + 3 deve ser igual a 5', () => {
+    expect(soma(2,3)).toBe(5)
+})
