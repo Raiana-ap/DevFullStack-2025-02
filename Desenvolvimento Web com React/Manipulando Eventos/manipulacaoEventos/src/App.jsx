a07823135a8fabab9a7883763c1e3e89ea2f37d2
Raiana Arbusti <raianaapaludo@gmail.com>
2025-10-14 12:14:45 -0300

    Manipulação de eventos

--- Desenvolvimento Web com React/Manipulando Eventos/manipulacaoEventos/src/App.jsx
+++ Desenvolvimento Web com React/Manipulando Eventos/manipulacaoEventos/src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
+import Product from "./components/Product"
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-     
-    </>
-  )
+  return < Product />
 }
 
 export default App
