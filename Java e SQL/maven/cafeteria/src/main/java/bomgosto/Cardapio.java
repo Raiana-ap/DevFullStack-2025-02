bdedec37ea56381f492cfa3ca9fca092c46e0f56
Raiana Arbusti <raianaapaludo@gmail.com>
2025-10-31 21:53:18 -0300

    Classes da cafeteria

---
+++ Java e SQL/maven/cafeteria/src/main/java/bomgosto/Cardapio.java
@@ -0,0 +1,5 @@
+package bomgosto;
+
+public class Cardapio {
+    
+}
---
+++ Java e SQL/maven/cafeteria/src/main/java/bomgosto/Main.java
@@ -0,0 +1,7 @@
+package bomgosto;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
